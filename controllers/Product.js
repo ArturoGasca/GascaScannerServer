4c4de2bcbff7955b9ba2c9b5e0d9588d54908e67
ArturoGasca <arturo.gasca@aliomexico.com>
2017-11-19 13:17:43 -0600

    _

--- controllers/Product.js
+++ controllers/Product.js
@@ -4,7 +4,7 @@ function getItem(req, res, next){
   Product.findById(req.params.id)
   .then(item => {
     if (item){
-      res.status(201).json(newItem)
+      res.status(201).json(item)
     }else{
       res.status(404).json("Barcode not found")
     }
